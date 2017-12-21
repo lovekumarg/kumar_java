17ceafdcdcc549ac9ccebf4528c332551bec03af
kumar <glovekumar123@gmail.com>
2017-12-21 17:39:51 +0530

    two program added

---
+++ kumar.java
@@ -0,0 +1,9 @@
+public class kumar
+{
+public static void main(String[] ags)
+{
+System.out.println("this sample java progarm");
+System.out.println("this sample java progarm");
+System.out.println("this sample java progarm");
+}
+}
